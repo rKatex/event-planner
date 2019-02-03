88fe952aa41e81607cd7b0b1c3cddb4223be89e9
Rene Kathofer <rene.kathofer@hotmail.com>
2019-02-03 08:01:07 -0800

    fixed json handling

--- backend/main.py
+++ backend/main.py
@@ -1,22 +1,26 @@
-import json
-import datetime
-
-from bson.objectid import ObjectId
 from flask import Flask
+from flask.json import JSONEncoder
 from flask_restful import Api
 from flask_pymongo import PyMongo
 from services.event import Event
+from bson import ObjectId
+
+class MongoJSONEncoder(JSONEncoder):
+    def default(self, o):
+        if isinstance(o, ObjectId):
+            return str(o)
+        return JSONEncoder.default(self, o)
+
+app = Flask(__name__)
+app.json_encoder=MongoJSONEncoder
+app.config["MONGO_URI"] = "mongodb://localhost:27017/event-board"
+mongo = PyMongo(app) 
 
 def addMongoResource(api, resource, *urls):
     api.add_resource(resource, *urls,  resource_class_kwargs={ 'mongo': mongo })
 
-app = Flask(__name__)
-app.config["MONGO_URI"] = "mongodb://localhost:27017/event-board"
-app.json_encoder = json.JSONEncoder
-mongo = PyMongo(app)
-
 api = Api(app)
 addMongoResource(api, Event, '/event')
-
+ 
 if __name__ == '__main__':
     app.run(debug=True) 
--- backend/services/event.py
+++ backend/services/event.py
@@ -1,15 +1,18 @@
 from .mongoresource import MongoResource
 from flask import jsonify
+from flask_restful import fields, marshal_with
+
+event_fields = {
+    'name':   fields.String,
+    'description':    fields.String,
+}
+
 
 class Event(MongoResource): 
     def get(self):
-        events = []
-        for event in self.mongo.db.events.find():
-            events.append(event)
-        print("xxxxxxxxxxxxxxxx")
-        print(events)
-        y =  jsonify(events)
-        print(y)
-        return y
-    def post(self):
-        return "yuhu"
+        return jsonify(list(self.mongo.db.events.find()))
+
+    @marshal_with(event_fields)
+    def post(self, event):
+        print(event)
+        #self.mongo.db.events.insert_one()
